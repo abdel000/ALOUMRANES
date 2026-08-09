feb601533400c29b0f723d83b36cd01f66f39470
abdel000 <aamraoui97@gmail.com>
2026-08-09 19:58:55 +0200

    Install Google Ads gtag.js (AW-10786699280) alongside GTM

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -138,6 +138,7 @@ export const Route = createRootRouteWithContext<{ queryClient: QueryClient }>()(
 });
 
 const GTM_ID = "GTM-WDBRJG3M";
+const GOOGLE_ADS_ID = "AW-10786699280";
 
 function RootShell({ children }: { children: ReactNode }) {
   return (
@@ -146,6 +147,12 @@ function RootShell({ children }: { children: ReactNode }) {
         <script
           dangerouslySetInnerHTML={{
             __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);})(window,document,'script','dataLayer','${GTM_ID}');`,
+          }}
+        />
+        <script async src={`https://www.googletagmanager.com/gtag/js?id=${GOOGLE_ADS_ID}`} />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `window.dataLayer = window.dataLayer || [];function gtag(){dataLayer.push(arguments);}gtag('js', new Date());gtag('config', '${GOOGLE_ADS_ID}');`,
           }}
         />
         <HeadContent />
